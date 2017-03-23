e5d312e1b441b3f8fde1419c75ca7089076827a2
MrBuggysan <MrBuggySan@users.noreply.github.com>
2017-03-23 09:33:12 -0600

    Update BroadcastSystem.java

--- BroadcastSystem.java
+++ BroadcastSystem.java
@@ -7,7 +7,7 @@ public class BroadcastSystem implements Runnable{
   }
 
   //only one Broadcastsystem can exist
-  public static BroadcastSystem(){
+  public static BroadcastSystem getInstance(){
     if( bLimit < 1 ){
         BroadcastSystem broadcastSystem = new BroadcastSystem();
         bLimit = 1;
